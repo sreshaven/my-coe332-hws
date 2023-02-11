f1298d5fa956a0928bf94151acd4d23d3c3cdafa
Sresha Ventrapragada <sreshaven@gmail.com>
2023-02-11 04:14:01 +0000

    finished writing the scripts

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -1,4 +1,5 @@
 import requests
+import math
 TURBIDITY_THRESHOLD = 1.0
 DECAY_FACTOR = 0.02
 
@@ -13,6 +14,8 @@ def calc_turbidity(a0: float, I90: float) -> float:
     Returns:
         turbidity (float): turbidity value in NTU units (between 0 - 40)
     """
+    turbidity = a0 * I90
+    return turbidity
 
 def calc_min_time(T0: float) -> float:
     """
@@ -22,12 +25,36 @@ def calc_min_time(T0: float) -> float:
         T0 (float): the current turbidity value
     
     Returns:
-        b (float): the minimum amount of time in hours
+        min_time (float): the minimum amount of time in hours
     """
+    if (T0 <= TURBIDITY_THRESHOLD):
+        return 0
+    else:
+        min_time = math.log(TURBIDITY_THRESHOLD/T0, 1 - DECAY_FACTOR)
+        return round(min_time, 2)
+
 
 def main():
     response = requests.get(url='https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json')
     turb_data = response.json()
     
+    # calculate average turbidity
+    recent_data = turb_data['turbidity_data'][-5:]
+    avg_turb = 0.0
+    for turb_dict in recent_data:
+        cur_a0 = turb_dict['calibration_constant']
+        cur_I90 = turb_dict['detector_current']
+        avg_turb += calc_turbidity(cur_a0, cur_I90)
+    avg_turb /= 5
+
+    # print info about recent turbidity
+    print('Average turbidity based on most recent five measurements = {:.4f} NTU'.format(avg_turb))
+    if (avg_turb > TURBIDITY_THRESHOLD):
+        print('Warning: Turbidity is above threshold for safe use')
+        print('Minimum time required to return below a safe threshold = {:.2f} hours'.format(calc_min_time(avg_turb)))
+    else:
+        print('Info: Turbidity is below threshold for safe use')
+        print('Minimum time required to return below a safe threshold = 0 hours')
+
 if __name__ == '__main__':
     main()
--- homework03/test_analyze_water.py
+++ homework03/test_analyze_water.py
@@ -1,8 +1,11 @@
 from analyze_water import calc_turbidity, calc_min_time
+import math
 import pytest
 
 def test_calc_turbidity():
-    assert calc_turbidity() == 
+    assert calc_turbidity(1.022, 1.137) == 1.022 * 1.137
+    assert calc_turbidity(1.047, -1.115) == 1.047 * -1.115
 
 def test_calc_min_time():
-    assert calc_min_time() ==
+    assert calc_min_time(0.25) == 0
+    assert calc_min_time(1.1992) == 8.99
