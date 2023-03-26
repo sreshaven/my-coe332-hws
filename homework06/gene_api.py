a0edb42a16cdd9f1e9ffd772e36d37110c6821b9
Sresha Ventrapragada <sreshaven@gmail.com>
2023-03-26 14:55:24 +0000

    adding files for homework 6

--- homework06/gene_api.py
+++ homework06/gene_api.py
@@ -1,20 +1,19 @@
 from flask import Flask, request
 import requests
 import redis
+import csv
 
 app = Flask(__name__)
 
 def get_redis_client():
-    return redis.Redis(host='redis-db', port=6379, db=0, decode_responses = True)
+    return redis.Redis(host = 'redis-db', port = 6379, db = 0, decode_responses = True)
 
 rd = get_redis_client()
-keys_r = requests.get('https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt')
-keys_list = keys_r.text.split('\n')[0].split('\t')
 
 @app.route('/data', methods=['POST','GET','DELETE'])
 def handle_data() -> list:
     """
-    If methods is POST, loads the HGNC data set into redis and returns message
+    If methods is POST, loads the auto trends data set into redis and returns message
     If method is GET, returns data from redis
     If method is DELETE, clears the data in redis and returns message
 
@@ -22,57 +21,100 @@ def handle_data() -> list:
         no arguments
 
     Returns:
-        hgnc_data (list): (if method is GET) list of dictionaries of the gene data in redis
+        auto_data (list): (if method is GET) list of dictionaries of the cars data in redis
     """
     if request.method == 'POST':
-        response = requests.get('https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json')
-        for item in response.json()['response']['docs']:
-            rd.hset(item['hgnc_id'], mapping = item)
-        return 'HGNC data is loaded into Redis\n'
+        with open('auto_trends_data.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                item = dict(row)
+                key = item['Manufacturer'] + '-' + item['Model Year'] + '-' + item['Vehicle Type']
+                rd.hset(key, mapping = item)
+        return 'Auto Trends data is loaded into Redis\n'
     elif request.method == 'GET':
-        hgnc_data = []
+        auto_data = []
         for key in rd.keys():
-            hgnc_data.append(rd.hgetall(key))
-        return hgnc_data
+            auto_data.append(rd.hgetall(key))
+        return auto_data
     elif request.method == 'DELETE':
         rd.flushdb()
-        return 'HGNC data has been deleted from Redis\n'
+        return 'Auto Trends data has been deleted from Redis\n'
     else:
         return 'The method you tried does not work\n'
 
-@app.route('/genes', methods=['GET'])
-def get_genes() -> list:
+@app.route('/years', methods=['GET'])
+def get_years() -> list:
     """
-    Returns a list of the genes currently in redis for the HGNC database for the '/genes' route
+    Returns a list of the years currently in redis for the Auto Trends database for the '/years' route
     
     Args:
         no arguments
     
     Returns:
-        genes_list (list): list of strings of the hgnc_ids
+        years_list (list): list of strings of the Model Year
     """
-    genes_list = []
-    genes_list = rd.keys()
-    return genes_list
+    years_list = []
+    for key in rd.keys():
+        car_dict = rd.hgetall(key)
+        if car_dict['Model Year'] not in years_list:
+            years_list.append(car_dict['Model Year'])
+    return years_list
 
-@app.route('/genes/<hgnc_id>', methods=['GET'])
-def get_gene_info(hgnc_id: str) -> dict:
+@app.route('/years/<year>', methods=['GET'])
+def get_year_info(year: str) -> list:
     """
-    Returns a dictionary with information about the specific gene if found in the HGNC database for the '/genes/<hgnc_id>' route
+    Returns a list with all cars from that year if found in the Auto Trends database for the '/years/<year>' route
 
     Args:
-        hgnc_id (str): the HGNC ID for the gene
+        year (str): the string of a Model Year
 
     Returns:
-        gene_info (dict): dictionary with information about the specified gene, if gene not found, will be empty dict
+        year_cars (list): list with cars from the specified year, if year not found, will be empty list
     """
-    gene_info = {}
-    if hgnc_id in rd.keys():
-        gene_info = rd.hgetall(hgnc_id)
-        for key in keys_list:
-            if key not in gene_info:
-                gene_info[key] = ""
-    return gene_info
+    year_cars = []
+    if year in get_years():
+        for key in rd.keys():
+            car_dict = rd.hgetall(key)
+            if car_dict['Model Year'] == year:
+                year_cars.append(car_dict)
+    return year_cars
+
+@app.route('/manufacturers', methods=['GET'])
+def get_manufacturers() -> list:
+    """
+    Returns a list of the manufacturers currently in redis for the Auto Trends database for the '/manufacturers' route
+
+    Args:
+        no arguments
+
+    Returns:
+        manufacturers_list (list): list of strings of the Manufacturer
+    """
+    manufacturers_list = []
+    for key in rd.keys():
+        car_dict = rd.hgetall(key)
+        if car_dict['Manufacturer'] not in manufacturers_list:
+            manufacturers_list.append(car_dict['Manufacturer'])
+    return manufacturers_list
+
+@app.route('/manufacturers/<manufacturer>', methods=['GET'])
+def get_manufacturer_info(manufacturer: str) -> list:
+    """
+    Returns a list with all cars from that manufacturer if found in the Auto Trends database for the '/manufacturers/<manufacturer>' route
+
+    Args:
+        manufacturer (str): the string of a Manufacturer
+
+    Returns:
+        manufacturer_cars (list): list with cars from the specified manufacturer, if manufacturer not found, will be empty list
+    """
+    manufacturer_cars = []
+    if manufacturer in get_manufacturers():
+        for key in rd.keys():
+            car_dict = rd.hgetall(key)
+            if car_dict['Manufacturer'] == manufacturer:
+                manufacturer_cars.append(car_dict)
+    return manufacturer_cars
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
